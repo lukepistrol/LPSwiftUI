bc1e549d83d0c4e25824c3637bdcabbb350e234b
Luke <l.pistrol@gmail.com>
2022-02-19 01:04:05 +0100

    Added Preview Devices statics

---
+++ Sources/LPSwiftUI/Extensions/Preview/PreviewDevice+Convenience.swift
@@ -0,0 +1,71 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lukas Pistrol on 19.02.22.
+//
+
+import SwiftUI
+
+public extension PreviewDevice {
+	// MARK: - iPhone 13 Series
+	static let iPhone13Mini: Self = "iPhone 13 mini"
+	static let iPhone13: Self = "iPhone 13"
+	static let iPhone13Pro: Self = "iPhone 13 Pro"
+	static let iPhone13ProMax: Self = "iPhone 13 Pro Max"
+
+	// MARK: - iPhone 12 Series
+	static let iPhone12Mini: Self = "iPhone 12 mini"
+	static let iPhone12: Self = "iPhone 12"
+	static let iPhone12Pro: Self = "iPhone 12 Pro"
+	static let iPhone12ProMax: Self = "iPhone 12 Pro Max"
+
+	// MARK: - iPhone 11 Series
+	static let iPhone11: Self = "iPhone 11"
+	static let iPhone11Pro: Self = "iPhone 11 Pro"
+	static let iPhone11ProMax: Self = "iPhone 11 Pro Max"
+
+	// MARK: - iPhone XS Series
+	static let iPhoneXS: Self = "iPhone XS"
+	static let iPhoneXSMax: Self = "iPhone XS max"
+	static let iPhoneXR: Self = "iPhone XR"
+
+	// MARK: - iPhone X Series
+	static let iPhoneX: Self = "iPhone X"
+
+	// MARK: - iPhone SE Series
+	static let iPhoneSE1: Self = "iPhone SE (1st generation)"
+	static let iPhoneSE2: Self = "iPhone SE (2nd generation)"
+
+	// MARK: iPad Pro 12.9
+	static let iPadPro12_5: Self = "iPad Pro (12.9-inch) (5th generation)"
+	static let iPadPro12_4: Self = "iPad Pro (12.9-inch) (4th generation)"
+	static let iPadPro12_3: Self = "iPad Pro (12.9-inch) (3rd generation)"
+	static let iPadPro12_2: Self = "iPad Pro (12.9-inch) (2nd generation)"
+	static let iPadPro12_1: Self = "iPad Pro (12.9-inch) (1st generation)"
+
+	// MARK: iPad Pro 11.0
+	static let iPadPro11_3: Self = "iPad Pro (11-inch) (3rd generation)"
+	static let iPadPro11_2: Self = "iPad Pro (11-inch) (2nd generation)"
+	static let iPadPro11_1: Self = "iPad Pro (11-inch) (1st generation)"
+
+	// MARK: iPad Pro 10.5
+	static let iPadPro10: Self = "iPad Pro (10.5-inch)"
+
+	// MARK: iPad Pro 9.7
+	static let iPadPro9: Self = "iPad Pro (9.7-inch)"
+
+	// MARK: iPad Air
+	static let iPadAir4: Self = "iPad Air (4th generation)"
+	static let iPadAir3: Self = "iPad Air (3rd generation)"
+
+	// MARK: iPad Mini
+	static let iPadMini6: Self = "iPad mini (6th generation)"
+	static let iPadMini5: Self = "iPad mini (5th generation)"
+
+	// MARK: - iPad
+	static let iPad9: Self = "iPad (9th generation)"
+	static let iPad8: Self = "iPad (8th generation)"
+	static let iPad7: Self = "iPad (7th generation)"
+	static let iPad6: Self = "iPad (6th generation)"
+}
