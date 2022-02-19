b636f5cfe32fbc0b9ab91bb09ed59f8f3aa3980f
Luke <l.pistrol@gmail.com>
2022-02-19 01:15:11 +0100

    added docs

--- Sources/LPSwiftUI/Extensions/Preview/PreviewDevice+Convenience.swift
+++ Sources/LPSwiftUI/Extensions/Preview/PreviewDevice+Convenience.swift
@@ -9,63 +9,98 @@ import SwiftUI
 
 public extension PreviewDevice {
 	// MARK: - iPhone 13 Series
+	/// iPhone 13 Mini - Preview Device
 	static let iPhone13Mini: Self = "iPhone 13 mini"
+	/// iPhone 13 - Preview Device
 	static let iPhone13: Self = "iPhone 13"
+	/// iPhone 13 Pro - Preview Device
 	static let iPhone13Pro: Self = "iPhone 13 Pro"
+	/// iPhone 13 Pro Max - Preview Device
 	static let iPhone13ProMax: Self = "iPhone 13 Pro Max"
 
 	// MARK: - iPhone 12 Series
+	/// iPhone 12 Mini - Preview Device
 	static let iPhone12Mini: Self = "iPhone 12 mini"
+	/// iPhone 12 - Preview Device
 	static let iPhone12: Self = "iPhone 12"
+	/// iPhone 12 Pro - Preview Device
 	static let iPhone12Pro: Self = "iPhone 12 Pro"
+	/// iPhone 12 Pro Max - Preview Device
 	static let iPhone12ProMax: Self = "iPhone 12 Pro Max"
 
 	// MARK: - iPhone 11 Series
+	/// iPhone 11 - Preview Device
 	static let iPhone11: Self = "iPhone 11"
+	/// iPhone 11 Pro - Preview Device
 	static let iPhone11Pro: Self = "iPhone 11 Pro"
+	/// iPhone 11 Pro Max - Preview Device
 	static let iPhone11ProMax: Self = "iPhone 11 Pro Max"
 
 	// MARK: - iPhone XS Series
+	/// iPhone XS - Preview Device
 	static let iPhoneXS: Self = "iPhone XS"
+	/// iPhone XS Max - Preview Device
 	static let iPhoneXSMax: Self = "iPhone XS max"
+	/// iPhone XR - Preview Device
 	static let iPhoneXR: Self = "iPhone XR"
 
 	// MARK: - iPhone X Series
+	/// iPhone X - Preview Device
 	static let iPhoneX: Self = "iPhone X"
 
 	// MARK: - iPhone SE Series
+	/// iPhone SE (1st gen) - Preview Device
 	static let iPhoneSE1: Self = "iPhone SE (1st generation)"
+	/// iPhone SE (2nd gen) - Preview Device
 	static let iPhoneSE2: Self = "iPhone SE (2nd generation)"
 
 	// MARK: iPad Pro 12.9
+	/// iPad Pro 12.9 inch (5th gen) - Preview Device
 	static let iPadPro12_5: Self = "iPad Pro (12.9-inch) (5th generation)"
+	/// iPad Pro 12.9 inch (4th gen) - Preview Device
 	static let iPadPro12_4: Self = "iPad Pro (12.9-inch) (4th generation)"
+	/// iPad Pro 12.9 inch (3rd gen) - Preview Device
 	static let iPadPro12_3: Self = "iPad Pro (12.9-inch) (3rd generation)"
+	/// iPad Pro 12.9 inch (2nd gen) - Preview Device
 	static let iPadPro12_2: Self = "iPad Pro (12.9-inch) (2nd generation)"
+	/// iPad Pro 12.9 inch (1st gen) - Preview Device
 	static let iPadPro12_1: Self = "iPad Pro (12.9-inch) (1st generation)"
 
 	// MARK: iPad Pro 11.0
+	/// iPad Pro 11 inch (3rd gen) - Preview Device
 	static let iPadPro11_3: Self = "iPad Pro (11-inch) (3rd generation)"
+	/// iPad Pro 11 inch (2nd gen) - Preview Device
 	static let iPadPro11_2: Self = "iPad Pro (11-inch) (2nd generation)"
+	/// iPad Pro 11 inch (1st gen) - Preview Device
 	static let iPadPro11_1: Self = "iPad Pro (11-inch) (1st generation)"
 
 	// MARK: iPad Pro 10.5
+	/// iPad Pro 10.5 inch - Preview Device
 	static let iPadPro10: Self = "iPad Pro (10.5-inch)"
 
 	// MARK: iPad Pro 9.7
+	/// iPad Pro 9.7 inch - Preview Device
 	static let iPadPro9: Self = "iPad Pro (9.7-inch)"
 
 	// MARK: iPad Air
+	/// iPad Air (4th gen) - Preview Device
 	static let iPadAir4: Self = "iPad Air (4th generation)"
+	/// iPad Air (3rd gen) - Preview Device
 	static let iPadAir3: Self = "iPad Air (3rd generation)"
 
 	// MARK: iPad Mini
+	/// iPad Mini (6th gen) - Preview Device
 	static let iPadMini6: Self = "iPad mini (6th generation)"
+	/// iPad Mini (5th gen) - Preview Device
 	static let iPadMini5: Self = "iPad mini (5th generation)"
 
 	// MARK: - iPad
+	/// iPad (9th gen) - Preview Device
 	static let iPad9: Self = "iPad (9th generation)"
+	/// iPad (8th gen) - Preview Device
 	static let iPad8: Self = "iPad (8th generation)"
+	/// iPad (7th gen) - Preview Device
 	static let iPad7: Self = "iPad (7th generation)"
+	/// iPad (6th gen) - Preview Device
 	static let iPad6: Self = "iPad (6th generation)"
 }
