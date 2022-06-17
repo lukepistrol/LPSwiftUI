17565318d62f4ad17edb358f254fcec79dc93700
Lukas Pistrol <lukas@pistrol.com>
2022-06-17 16:20:51 +0200

    added CameraPicker for iOS16

--- Sources/LPSwiftUI/LPImagePicker/LPImagePicker.swift
+++ Sources/LPSwiftUI/LPImagePicker/LPImagePicker.swift
@@ -9,6 +9,86 @@
 import SwiftUI
 import PhotosUI
 
+@available(iOS 16, *)
+public struct LPCameraPicker<Content: View>: View {
+
+    private var content: Content
+    @State private var displayCamera: Bool = false
+    @Binding private var selection: CameraPickerItem?
+
+    public init(_ selection: Binding<CameraPickerItem?>, content: @escaping () -> Content) {
+        self._selection = selection
+        self.content = content()
+    }
+
+    public var body: some View {
+        Button {
+            displayCamera.toggle()
+        } label: {
+            content
+        }
+        .fullScreenCover(isPresented: $displayCamera) {
+            LPCameraPickerRepresentable($selection)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+@available(iOS 16, *)
+internal struct LPCameraPickerRepresentable: UIViewControllerRepresentable {
+
+    @Binding private var selection: CameraPickerItem?
+
+    init(_ selection: Binding<CameraPickerItem?>) {
+        self._selection = selection
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+        private let parent: LPCameraPickerRepresentable
+
+        init(_ parent: LPCameraPickerRepresentable) {
+            self.parent = parent
+        }
+
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+        ) {
+            if let image = info[.originalImage] as? UIImage,
+               let data = image.pngData() {
+                parent.selection = CameraPickerItem(imageData: data)
+                picker.dismiss(animated: true)
+            }
+        }
+    }
+}
+
+@available(iOS 16, *)
+public struct CameraPickerItem: Codable, Transferable, Equatable {
+
+    public static var transferRepresentation: some TransferRepresentation {
+        CodableRepresentation(contentType: .image)
+    }
+
+    public let imageData: Data
+}
+
+
 /// An Image Picker to pick images from photos
 @available(iOS, deprecated: 16.0, message: "Use PhotosUI's `PhotosPicker` instead")
 public struct LPImagePicker: UIViewControllerRepresentable {
@@ -73,3 +153,4 @@ public extension LPImagePicker {
 	}
 }
 #endif
+
